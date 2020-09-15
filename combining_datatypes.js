c21f768843ab3ca5aa4d7759afdf8015319823ac
Juliana Fernandes <julianafernandes@Albertos-MacBook-Pro.local>
2020-09-15 14:49:06 -0300

    Combine objects, arrays, and functions

---
+++ combining_datatypes.js
@@ -0,0 +1,36 @@
+
+//---------Combine objects, arrays, and functions----------
+//Question 1
+var crayonBox = {
+    crayons:["yellow","pink","blue"]
+  }
+  console.log(crayonBox.crayons[1])
+  
+//Question 2
+  
+  var bottle = {
+     cap: {
+      material: "plastic",
+      color: "blue",
+      beverage: "water"
+    }
+  }
+  
+  console.log(bottle.cap.material)
+  
+  //Question 3
+  receipt = [
+     {
+      name: "bread",
+      price: 9.99
+    }
+  ]
+  
+  console.log(receipt[0].name)
+  
+  //Question 4
+  apartmentBuilding  = [
+    tenants = ["Samuel","Daniel","Jessie"]
+  ]
+  
+  console.log(apartmentBuilding[0][1])
